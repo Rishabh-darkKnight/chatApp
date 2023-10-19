766655fa1cb62e663265d28eeb7b2e6e03355483
Rishi <rishi8890484418@gmail.com>
2023-10-19 17:44:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ws = new WebSocket('ws://localhost:3030');
+
+  useEffect(() => {
+    ws.onmessage = (event) => {
+      const receivedMessage = event.data;
+      setMessages([...messages, receivedMessage]);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [messages, ws]);
+
+  const sendMessage = () => {
+    ws.send(message);
+    setMessage('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chat-container">
+        <div className="chat-messages">
+          {messages.map((msg, index) => (
+            <div key={index}>{msg}</div>
+          ))}
+        </div>
+        <div className="chat-input">
+          <input
+            type="text"
+            placeholder="Type your message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
